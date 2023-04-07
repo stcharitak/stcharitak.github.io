e7d74c3975dfbec13280825f39abc3f2245921b0
Stavros Charitakis <st.charitak@gmail.com>
2023-04-07 16:22:16 +0200

    remove everything

--- src/components/BlockTitle.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BlockTitle(props) {
-  return (
-    <div className="block-title">
-      <h2>{props.title}</h2>
-    </div>
-  );
-}
--- src/components/ButtonList.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ButtonList(props) {
-  return <div className="home-buttons">{props.children}</div>;
-}
